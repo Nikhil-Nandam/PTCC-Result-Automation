56ef2a86d995d43e0e52374014f228af8d0370cb
Nikhil Nandam <nikhil.nandam@gmail.com>
2022-02-01 21:21:33 +0530

    remove preprocessing as it is already done in quiz_emails.py

--- main.py
+++ main.py
@@ -10,51 +10,21 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 
-# handle quiz - 1
-quiz_1 = pd.read_csv('responses/Quiz - 1 (Responses).csv')
-emails_1 = quiz_1[['Email Address', 'Score']]
-emails_1.rename(columns={'Score': 'Quiz-1 Score'}, inplace=True)
-
-
-# handle quiz - 2
-quiz_2 = pd.read_csv('responses/Quiz - 2 (Responses).csv')
-emails_2 = quiz_2[['Email Address', 'Name', 'Score']]
-emails_2.rename(columns={'Score': 'Quiz-2 Score'}, inplace=True)
-
-# merge quiz_1 and quiz_2 on common emails
-emails_names = pd.merge(emails_1, emails_2, how='inner', on='Email Address')
-emails_names.to_csv('responses/Quiz 1 & 2 participants.csv')
-
-
-# handle quiz - 3
-quiz_3 = pd.read_csv('responses/Quiz - 3 (Responses).csv')
-quiz_3.rename(columns={'Name (Actual name that you want on the certificate)': 'Name', 'Score': 'Quiz-3 Score'}, inplace=True)
-emails_3 = quiz_3[['Email Address', 'Quiz-3 Score', 'Name']]
-emails_names.drop(columns=['Name'], inplace=True)
-
-# merge emails_names and quiz_3 on common emails
-emails_names = pd.merge(emails_names, emails_3, how='inner', on='Email Address')
-
-
-# required columns
-cols = ['Email Address', 'Name', 'Quiz-1 Score', 'Quiz-2 Score', 'Quiz-3 Score']
-emails_names = emails_names[cols]
-
-emails_names.to_csv('responses/Quiz 1,2,3 participants.csv')
-
 # credentials of sender and address of receiver
-sender_address = 'emailforclasses22@gmail.com'
+sender_address = 'ccjntuhceh@gmail.com'
 sender_password = getpass('Enter password: ')
 
 pwd = os.getcwd()
 
 # change coordinates to your liking (where the text should be placed) 
-bottom_right_corner = (1250, 550)
-top_left_corner = (200, 400)
+# bottom_right_corner = (1250, 550)
+# top_left_corner = (200, 400)
+bottom_right_corner = (2400, 1300)
+top_left_corner = (800, 1000)
 
 
 # open CSV file in read mode
-df = pd.read_csv("responses/sample_names.csv")
+df = pd.read_csv("responses/Quiz 1,2,3,4.csv")
 
 for index, row in df.iterrows():
     name = row['Name']
@@ -90,8 +60,8 @@ for index, row in df.iterrows():
     # Save the edited image
     im = img.convert('RGB')
     im.save(pwd + '/certs/pdfs/' + name + '.pdf', "PDF", resolution=100.0)
+    # im.save(pwd + '/certs/pngs/' + name + '.png')
     print("Created: " + name + "-cert.pdf")
-
 
     # use other fields from each row
     email = row['Email Address']
